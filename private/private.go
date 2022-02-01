e9c412ce388b003d10de42b00e840e07de2b3f5b
Austin Green <austinjgreen94@yahoo.com>
2022-01-31 23:30:19 -0500

    feat: rename internal to private and move them to top level

--- private/private.go
+++ private/private.go
@@ -1,13 +1,13 @@
-package internal
+package private
 
-// Error (the internal.Error type) is used to differentiate a usual execution error from one that contains internal
+// Error (the private.Error type) is used to differentiate a usual execution error from one that contains internal
 // data which shouldnt leak to public endpoints. This has little extra functionality besides a typical error, but is
 // useful for checking via errors.Is() to see if an error contains an internal message.
 type Error struct {
 	internalErr error
 }
 
-func NewInternalError(err error) Error {
+func NewPrivateError(err error) Error {
 	return Error{
 		internalErr: err,
 	}
